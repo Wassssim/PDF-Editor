ba65a65059bca7b6da03282b0996cf4dc3262759
Wassssim <aprsn124@gmail.com>
2022-11-20 14:41:13 +0100

    initial commit

---
+++ pdf-editor-frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from 'react'
+import { FileWithPath } from "React-dropzone";
+import './App.css'
+import UploadDropZone from './UploadDropZone/UploadDropZone'
+
+function App() {
+  const [file, setFile] = useState<FileWithPath | null>(null)
+
+  const onFileAdded = useCallback((acceptedFiles: FileWithPath[]) => {
+    console.log(acceptedFiles);
+    setFile(acceptedFiles[0]);
+    return acceptedFiles[0];
+  }, []);
+
+  return (
+    <div className="App">
+      <div className='dropzone'>
+        <UploadDropZone onFileAdded={(acceptedFiles) => onFileAdded(acceptedFiles)}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ pdf-editor-frontend/src/UploadDropZone/UploadDropZone.tsx
@@ -0,0 +1,53 @@
+import { FileWithPath, useDropzone } from "React-dropzone";
+import './UploadDropZone.css';
+
+interface UploadDropZoneProps {
+    onFileAdded: (files: FileWithPath[]) => FileWithPath;
+}
+
+function UploadDropZone({ onFileAdded } : UploadDropZoneProps) {
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles  } = useDropzone({
+        accept: {
+            'application/pdf': ['.pdf'],
+        },
+        onDrop: (acceptedFiles) => onFileAdded(acceptedFiles),
+        multiple: false,
+    });
+
+    const files = acceptedFiles.map((file: FileWithPath) => (
+        <div key={file.path} className="file-description">
+          {file.path} - {file.size} bytes
+        </div>
+    ));
+
+    return (
+        <div 
+            {...getRootProps({ 
+                className: `upload-dropzone ${isDragActive?'active':''}`,
+                onClick: event => {
+                    if (event.target.type != 'button') {
+                        event.stopPropagation();
+                    }
+                }
+            })}
+        >
+            <input className="input-zone" {...getInputProps({})} />
+            <div className="text-center">
+                <p className="dropzone-content">
+                    {isDragActive 
+                        ? 
+                            ("Release to drop the files here") 
+                        : 
+                            ("Drag and drop PDF file here, or click to select it")
+                    }
+                </p>
+                <button type="button">
+                Click to select files
+                </button>
+                {files}
+            </div>
+        </div>
+    )
+}
+
+export default UploadDropZone;
