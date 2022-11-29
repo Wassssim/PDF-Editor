cdf684ce6164580246d88c98e4e66b38a7781263
Wassssim <aprsn124@gmail.com>
2022-11-30 00:18:42 +0100

    nginx/navbar hotfix

--- pdf-editor-frontend/src/components/NavBar/NavBar.tsx
+++ pdf-editor-frontend/src/components/NavBar/NavBar.tsx
@@ -2,16 +2,18 @@ import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select"
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
 	const { lang } = useParams();
+	const navigate = useNavigate();
     const { i18n } = useTranslation("translations", { useSuspense: true });
 
     const handleChange = (event: SelectChangeEvent) => {
-			i18n.changeLanguage(event.target.value as string)
-			.catch(err => console.error(err));
+		navigate(`/${event.target.value as string}`);
+		/*i18n.changeLanguage(event.target.value as string)
+		.catch(err => console.error(err));*/
     };
 
 	useEffect(() => {
