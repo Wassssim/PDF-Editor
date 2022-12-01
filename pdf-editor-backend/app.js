559fbea25a4abf388d6d7386457d951d7bfa51ae
Wassssim <aprsn124@gmail.com>
2022-12-01 14:55:22 +0100

    Improved upload/download and cleaned up node backend

--- pdf-editor-backend/app.js
+++ pdf-editor-backend/app.js
@@ -5,27 +5,27 @@ const cors = require('cors');
 const _ = require('lodash'); // TODO: check if this is still used
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const multer = require('multer');
+const multer = require('multer');
 const { writeFile, readFile } = require('fs').promises;
 const PdfFile = require('./models/PdfFile');
 const mongoose = require('mongoose');
-
+const logger = require('./logging/logger');
 
 // mongodb
 mongoose.connect(process.env.DATABASE_URL)
-    .then(() => console.log("connected to mongodb"))
-    .catch((err) => console.error("Error connecting to mongodb: ", err.message));
+    .then(() => logger.info("connected to mongodb"))
+    .catch((err) => logger.error("Error connecting to mongodb: ", err.message));
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // enable files upload
 // TODO: decide between multer and fileUpload
-//const upload = multer({dest: "storage/uploads"});
+const upload = multer({dest: "storage/uploads"});
 
-app.use(fileUpload({
+/*app.use(fileUpload({
     createParentPath: true
-}));
+}));*/
 
 // enables access to body in request
 app.use(bodyParser.json());
@@ -38,42 +38,52 @@ app.use(cors({
     origin: '*'
 }));
 
-app.post('/api/pdf-file/upload', async (req, res) => {
+const whitelist = [
+    'application/pdf'
+];
+
+app.post('/api/pdf-file/upload', upload.single("pdf_file"), async (req, res) => {
     try {
-        if(!req.files) {
-            res.send({
+        logger.debug("uploading pdf file: ", req.file);
+        if (!req.file) {
+            return res.send({
                 status: false,
                 message: 'No file uploaded'
             });
-        } else {
-            pdf_file = req.files.pdf_file;
-            // TODO randomize file names
-            // save file to storage
-            await writeFile(`./storage/uploads/${pdf_file.name}`, pdf_file.data); 
-            fileData = {
-                path: `storage/uploads/${pdf_file.name}`,
-                originalName: pdf_file.name
-            };
-
-            // add new entry to databse
-            const pdfFile = await PdfFile.create(fileData);
-            
-            //send response
-            res.send({
-                status: true,
-                message: 'PDF File is uploaded',
-                data: {
-                    id: pdfFile.id,
-                    name: pdf_file.name,
-                    mimetype: pdf_file.mimetype,
-                    size: pdf_file.size,
-                    link: `${req.headers.host}/api/pdf-file/${pdfFile.id}`
-                }
-            });        
         }
+
+        if (!whitelist.includes(req.file.mimetype)) {
+            return res.send({
+                status: false,
+                message: 'Invalid file type'
+            });
+        }
+
+        // save file to storage
+        fileData = {
+            path: req.file.path,
+            originalName: req.file.originalname
+        };
+
+        // add new entry to databse
+        const pdfFile = await PdfFile.create(fileData);
+        
+        //send response
+        return res.send({
+            status: true,
+            message: 'PDF File is uploaded',
+            data: {
+                id: pdfFile.id,
+                name: req.file.originalname,
+                mimetype: req.file.mimetype,
+                size: req.file.size,
+                link: `${req.headers.host}/api/pdf-file/${pdfFile.id}` // TODO: check if this is correct
+            }
+        });        
+        
     } catch (err) {
-        console.log(err);
-        res.status(500).send({
+        logger.error(err);
+        return res.status(500).send({
             status: false,
             message: err
         });
@@ -83,14 +93,23 @@ app.post('/api/pdf-file/upload', async (req, res) => {
 
 // download pdf
 app.get('/api/pdf-file/:id', async (req, res) => {
-    // TODO: handle when id doesn't exist
     try {
+        // check if file exists
+        const count = await PdfFile.countDocuments({_id: req.params.id});
+
+        if (count === 0)
+            return res.status(404).send({
+                status: false,
+                message: "File not found"
+            });
+
+        // get file from database
         const pdfFile = await PdfFile.findById(req.params.id);
-        console.log(pdfFile)
-        res.download(pdfFile.path, pdfFile.originalName);
+
+        return res.download(pdfFile.path, pdfFile.originalName);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send({
+        logger.error(err);
+        return res.status(500).send({
             status: false
         });
     }
@@ -103,21 +122,21 @@ app.get('/api/assets/i18n/:lang/:ns', async (req, res) => {
 
     try {
         const data = await readFile(path, 'utf8');
-        res.send({
+        return res.send({
             status: true,
             data: JSON.parse(data)
         });
     } catch (err) {
-        res.status(500).json({
+        return res.status(500).json({
             status: false,
             message: err
         });
     }
-})
+});
 
-
+// run server
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`);
+    logger.info(`app listening at http://localhost:${port}`);
 });
 
 // TODO: Graceful shutdown
